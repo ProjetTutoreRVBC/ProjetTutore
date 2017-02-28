3e8b911d49e8e6cadeb490301c9feb2c7c426af0
simonvasseur91550 <simonvasseur91550@live.com>
2017-02-28 16:50:15 -0500

    new vue

--- app/Resources/views/View/homepageTest.html.php
+++ app/Resources/views/View/homepageTest.html.php
@@ -1,17 +1,48 @@
-<!-- app/Resources/views/View/homepageTest.html.php--> 
+<!-- app/Resources/views/View/homepageTest.html.php-->
 <!doctype html>
+
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>nostream</title>
-    <link rel="stylesheet" href="/web/bundles/framework/css/foundation.css">
-    <script type="text/javascript" src="/web/bundles/framework/js/top-bar.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.0.1/js/vendor/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.0.1/js/foundation.min.js"></script>
-    <script type="text/javascript" src="<?php echo $view['assets']->getUrl('bundles/framework/js/validation/form.js') ?>"></script>
-    <script type="text/javascript" src="<?php echo $view['assets']->getUrl('bundles/framework/js/display/upload.js') ?>"></script>
-    <script> 
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>nostream</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script language="JavaScript" type="text/javascript" src="<?php echo $view['assets']->getUrl('bundles/framework/js/deep-purple.js') ?>"></script>
+  <script language="JavaScript" type="text/javascript" src="<?php echo $view['assets']->getUrl('bundles/framework/js/cool-buttons.js') ?>"></script>
+  <link rel="stylesheet" href="/web/bundles/framework/css/deep-purple.css">
+  <link rel="stylesheet" href="/web/bundles/framework/css/cool-buttons.css">
+  <script>
+    $(document).ready(function() {
+      var item_num = $('nav li').length + 1;
+      var btn_state = true;
+
+      $('nav li').hover(function() {
+        $(this).addClass('hover');
+      }, function() {
+        $(this).removeClass('hover');
+      });
+
+      $('nav li').on('click', function() {
+        if (btn_state) {
+          btn_state = !btn_state;
+          $('nav li').removeClass('currentPage');
+          $(this).addClass('currentPage');
+
+          var i = $('nav li').index(this);
+          $('article').removeClass('show');
+          $('article').addClass('hide');
+          $('article').eq(i).addClass('show');
+
+          setTimeout(function() {
+            btn_state = !btn_state;
+          }, 500);
+        }
+      });
+
+
+    });
+  </script>
+  <!--<script> 
       function suggest(str) {
       if (str.length == 0) { 
         document.getElementById("sugg_list").style.display = "none";
@@ -39,130 +70,95 @@
       
         }
       }
-    </script>  
-    <link rel="stylesheet" href="/web/bundles/framework/css/top-bar.css">
+    </script>  -->
 </head>
 
-<body onresize="handleWindow()" onload="handleWindow()">
-    <div class="top-bar">
-            <div style="display: inline-block;width:100%;">
-              <div id="left-search" style="float:left;">
-                <ul id="menu" class="menu">
-                    <li><a href="/web/app_dev.php/" ><img id="logo" class="" src="/web/bundles/framework/images/logo.png" alt="logo"></a></li>
-                    <li><input id ="search-bar" class="search-bar" type="input" placeholder="Search Here" onkeyup="suggest(this.value)"></li>
-                    <li><button id="search" class="button" type="button">Search</button></li>
-                </ul>
-              </div>
-                        <?php
-                          $href="login";
-                          $log="Login";
-                          if(isset($_COOKIE["pseudo"]) && !empty($_COOKIE["pseudo"]))
-                          {
-                            $href = "logout";
-                            $log = "Logout";
-                            echo '<div id ="right-log" style="float:right;">
-                            <ul id="menu" class="menu">';
-                            
-                            echo '<li id="signed"><a href="gestion"><img style="width:40px;height:40px;" alt="" src="/web/bundles/framework/images/param.png"></a></li>';
-                            echo '<li id="signed-1"><a href="profile/'.$user_page.'"><img style="width:40px;height:40px;" alt="" src="/web/bundles/framework/images/met.jpg"></a></li>';
-                          }else
-                          {
-                            echo '<div id ="right-log" style="float:right;">
-                            <ul id="menu" class="menu">';
-                            echo '<li id="signIn" ><a href="register"><button class="button" type="button">Sign Up</button></a></li>';
-                          }
-                            
-                          echo '<li id ="logIn">';
-                          echo '<a href="'.$href.'">';
-                          echo '<button  id="log" type ="button" class="button" >'.$log.'</button></a>';
-                        ?>
-                    </li>  
-                </ul>
-            </div>
-          </div>
-
-          <div id="suggestions" style="background-color:white;width:500px;margin-left:87px;margin-top:-5px;position:fixed;z-index:1;border-style: ridge 1px;">
-            <ul id="sugg_list" style="list-style:none;background-color:white;margin-left:0.25em;display:none;">
-              
-            </ul>
-          </div>
-            <div id="liste" class="liste">
-              <ul class="tabs " data-tabs id="tabs_example">
-                <li class="tabs-title "><a href="#tab2">Vidéastes</a></li>
-                <li class="tabs-title "><a href="#tab3 ">Chaînes</a></li>
-                <li class="tabs-title "><a href="#tab4 ">Actualité</a></li>
-                <li class="tabs-title "><a href="#tab5 ">Abonnements</a></li>
-                <li class="tabs-title is-active "><a href="#tab1">Tendances</a></li>
-              </ul>
-              </div>
-        </div>
-        <div  id="videos" class="tabs-content " data-tabs-content="tabs_example"  style="margin-top:130px;">
-        <!------------------------------------------------------------Section Tendances---------------------------->
-          <h4 id="completion">
-            
-          </h4>
-          <div  class="tabs-panel is-active " id="tab1" >
-            <div class ="defilement-video" style="text-align: center;">
-              <?php
-              foreach($video as $v)
-                {
-                  $titre= $v['nameVideo'];
-                  $id = $v['idVideo'];
-                  $date = $v['dateVideo'];
-                  $vues = $v['viewsVideo'];
-                  $Page = $page[$titre];
-                  $Channel = $channel[$titre];
-                  echo '
-                  <a href ="watch?v='.$id.'">
-                    <div style="height:125px;width:225px;display:inline-block;margin:4px;">
-                      <div style="height:35px;overflow:hidden;">
-                          <font size="2" class="titres"><strong>'.$titre.'</strong></font><br>
-                      </div>
-                      <img src="/web/bundles/framework/miniature/'.$v['miniature'].'" style="height:125px;width:225px;text-align:center;"><br>
-                      <div>
-
-                      </div>
-
-                      <div style="width:225px;">
-                          <a href="channel/'.$Channel.'" class="button tiny" style="margin-left:none;margin-right:none;width:48.5%;height:100%">
-                              <font size="1">'.$Channel.'</font>
-                          </a>
-                          <a href="profile/'.$Page.'" class="button tiny" style="margin-left:none;margin-right:none;width:48.5%;height:100%">
-                              <font size="1">'.$Page.'</font>
-                          </a>
-                      </div>
-                      <div style="text-align:center;">
-                          <font size="1 ">'.$vues.' vues -</font>
-                          <font size="1 ">le '.$date.'</font>
-                      </div>
-                  </div>
-                </a>';
-                }
-              ?>
-            </div>  
-          </div>
-        <!------------------------------------------------------------Section Vidéastes---------------------------->
-
-        <div class="tabs-panel" id="tab2">
-
-        </div>
-
-        <div class="tabs-panel " id="tab3">
-            
-        </div>
-        <div class="tabs-panel " id="tab4">
-            
-        </div>
-        <div class="tabs-panel " id="tab5">
-           
-        </div>
+<body>
+  <div class="wrapper">
+    <div class="header">
+      <a href="/web/app_dev.php/" style="width:100px;height:100px;border-radius: 8px;"><img id="logo" class="nostream" src="/web/bundles/framework/images/logo.png" alt="logo"></a>
+      <input type="text" class="searchbar" name="search" placeholder="Search..">
+      <?php
+          $href = "login";
+          $log  = "Login";
+          if (isset($_COOKIE["pseudo"]) && !empty($_COOKIE["pseudo"])) {
+              $href = "logout";
+              $log  = "Logout";
+              echo '<a href="gestion"><img style="margin-top:10px;width:40px;height:40px;float:right;margin-right:10px" alt="" src="/web/bundles/framework/images/param.png"></a></li>';
+              echo '<a href="profile/' . $user_page . '"><img style="margin-top:10px;width:40px;height:40px;float:right;margin-right:10px" alt="" src="/web/bundles/framework/images/met.jpg"></a></li>';
+          } else {
+              echo '<a href="register"><button class="btn-11" type="button">Sign Up</button></a>';
+          }
+          echo '<a href="' . $href . '">';
+          echo '<button id="log" type ="button" class="btn-11" >' . $log . '</button></a>';
+        ?>
+        <!--<a href="register"><button class="btn-11" type="button">Sign Up</button></a>
+        <a href="login"><button class="btn-11" type="button">Login</button></a>-->
     </div>
-    <script>
-        $(document).ready(function() {
-            $(document).foundation();
-        })
-    </script>
+    <nav>
+      <ul>
+        <li class="gnav1">Tendances</li>
+        <li class="gnav2">Abonnements</li>
+        <li class="gnav3">Vidéastes</li>
+        <li class="gnav4">Chaînes</li>
+      </ul>
+    </nav>
+    <div class="contents" id="contents">
+      <div class="container">
+        <article id="page1" class="show top">
+          <section>
+            <div class="defilement-video" style="text-align: center;">
 
+              <div style="height:125px;width:225px;display:inline-block;">
+                <div style="height:35px;overflow:hidden;">
+                  <font class="titres" style="font-size:12px;"><strong>Metallica - Atlas, Rise! Teaser from Hardwired...To self Destruct</strong></font><br>
+                </div>
+                <img src="/web/bundles/framework/images/atlas.jpg" style="border-radius:8px;height:125px;width:225px;text-align:center;"><br>
+                <div style="width:225px;margin-top:-2px;">
+                  <button class='myButt one'>
+                    <div class='insider'></div>
+                    <p style="margin-top:4px;font-size:10px;">Hover over me</p>
+                </button>
+                  <button class='myButt one'>
+                    <div class='insider'></div>
+                     <p style="margin-top:4px;font-size:10px;">Hover over me</p>
+                </button>
+                </div>
+                <div style="text-align:center;">
+                  <font size="1 ">1 234 992 vues -</font>
+                  <font size="1 ">le 29/10/16</font>
+                </div>
+              </div>
+            </div>
+          </section>
+        </article>
+        <article id="page2">
+          <section>
+            <h1>Tab2 Title</h1>
+            <p>This is tab two.</p>
+          </section>
+        </article>
+        <article id="page3">
+          <section>
+            <h1>Tab3 Title</h1>
+            <p>This is tab three.</p>
+          </section>
+        </article>
+        <article id="page4">
+          <section>
+            <h1>Tab4 Title</h1>
+            <p>This is tab four.</p>
+          </section>
+        </article>
+        <article id="page5">
+          <section>
+            <h1>Tab5 Title</h1>
+            <p>This is tab five</p>
+          </section>
+        </article>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
