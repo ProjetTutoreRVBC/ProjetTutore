605e51b1a74b950b0218ef794d25bbbddce74c9a
michael.reilhac <michael.reilhac@live.fr>
2017-03-07 15:21:45 -0500

    abonnement_channel

--- src/AppBundle/Controller/ChannelController.php
+++ src/AppBundle/Controller/ChannelController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 //...
+use AppBundle\Model\Page;
 use AppBundle\Model\Video;
 use AppBundle\Model\Channel;
 use AppBundle\Model\Nostreamer;
@@ -25,12 +26,23 @@ class ChannelController extends Controller
     public function showAction($slug)
     { 
       $user = new Channel();
+      $idPage = $user->getPageId($slug);
       $info_c = $user->getChannel($slug);
-      $subs =  $user->getSubsChannel($info_c['idChannel']);
+      $subs =  $user->getSubsChannel($idPage['idPage']);
       $video = new Video(); 
       $list_v = $video->getListVideo();
       $list_c = $video->getListChannelByIdVideo();
       $list_p = $video->getListPageByIdVideo();
-      return $this->render('View/channel.html.php',array("profile"=>$info_c['profile_img'],"banniere"=>$info_c['banniere_img'],"name_channel"=>$info_c['nameChannel'],"subs_channel"=>$subs['COUNT(*)'],"video" => $list_v,"channel"=>$list_c,"page"=>$list_p));
+      if(isset($_COOKIE['pseudo'])){  
+        if(isset($_POST['abonnement']) && $_POST != null){
+            $user_page = new Page();
+            $user = new Nostreamer();
+            $user_id = $user->getId($_COOKIE['pseudo']);
+            $user_page->addSubPage($user_id['idNostreamer'],$_POST['abonnement']);
+          }
+      }
+      return $this->render('View/channel.html.php',array("idPage"=>$idPage['idPage'],"profile"=>$info_c['profile_img'],"banniere"=>$info_c['banniere_img'],"name_channel"=>$info_c['nameChannel'],"subs_channel"=>$subs['COUNT(*)'],"video" => $list_v,"channel"=>$list_c,"page"=>$list_p));
     }
+  
+  
 }
--- var/cache/dev/appDevDebugProjectContainer.php
+++ var/cache/dev/appDevDebugProjectContainer.php
@@ -392,7 +392,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getCache_SystemService()
     {
-        return $this->services['cache.system'] = \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('s02+v9NxYk', 0, '9pPHQZTHSNu41kzQ566A0Q', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
+        return $this->services['cache.system'] = \Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('s02+v9NxYk', 0, '2hQec6MQE3eY2KVigtHIMA', (__DIR__.'/pools'), $this->get('monolog.logger.cache', ContainerInterface::NULL_ON_INVALID_REFERENCE));
     }
 
     /**
@@ -410,8 +410,8 @@ class appDevDebugProjectContainer extends Container
         $b = new \Symfony\Component\HttpKernel\CacheClearer\Psr6CacheClearer();
         $b->addPool($this->get('cache.app'));
         $b->addPool($this->get('cache.system'));
-        $b->addPool(\Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('QUiMwqaVfU', 0, '9pPHQZTHSNu41kzQ566A0Q', (__DIR__.'/pools'), $a));
-        $b->addPool(\Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('KFgWZO3Rl+', 0, '9pPHQZTHSNu41kzQ566A0Q', (__DIR__.'/pools'), $a));
+        $b->addPool(\Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('QUiMwqaVfU', 0, '2hQec6MQE3eY2KVigtHIMA', (__DIR__.'/pools'), $a));
+        $b->addPool(\Symfony\Component\Cache\Adapter\AbstractAdapter::createSystemCache('KFgWZO3Rl+', 0, '2hQec6MQE3eY2KVigtHIMA', (__DIR__.'/pools'), $a));
 
         return $this->services['cache_clearer'] = new \Symfony\Component\HttpKernel\CacheClearer\ChainCacheClearer(array(0 => $b));
     }
@@ -2244,7 +2244,7 @@ class appDevDebugProjectContainer extends Container
 
         $g = new \Symfony\Component\Security\Http\AccessMap();
 
-        return $this->services['security.firewall.map.context.main'] = new \Symfony\Bundle\SecurityBundle\Security\FirewallContext(array(0 => new \Symfony\Component\Security\Http\Firewall\ChannelListener($g, new \Symfony\Component\Security\Http\EntryPoint\RetryAuthenticationEntryPoint(80, 443), $a), 1 => new \Symfony\Component\Security\Http\Firewall\ContextListener($b, array(0 => $this->get('security.user.provider.concrete.database_users'), 1 => new \Symfony\Component\Security\Core\User\InMemoryUserProvider()), 'main', $a, $this->get('debug.event_dispatcher', ContainerInterface::NULL_ON_INVALID_REFERENCE), $c), 2 => new \Symfony\Component\Security\Guard\Firewall\GuardAuthenticationListener($this->get('security.authentication.guard_handler'), $d, 'main', array(0 => $e), $a), 3 => new \Symfony\Component\Security\Http\Firewall\AnonymousAuthenticationListener($b, '58bda8ed25b6f2.78595253', $a, $d), 4 => new \Symfony\Component\Security\Http\Firewall\AccessListener($b, $this->get('debug.security.access.decision_manager'), $g, $d)), new \Symfony\Component\Security\Http\Firewall\ExceptionListener($b, $c, new \Symfony\Component\Security\Http\HttpUtils($f, $f), 'main', $e, NULL, NULL, $a, false));
+        return $this->services['security.firewall.map.context.main'] = new \Symfony\Bundle\SecurityBundle\Security\FirewallContext(array(0 => new \Symfony\Component\Security\Http\Firewall\ChannelListener($g, new \Symfony\Component\Security\Http\EntryPoint\RetryAuthenticationEntryPoint(80, 443), $a), 1 => new \Symfony\Component\Security\Http\Firewall\ContextListener($b, array(0 => $this->get('security.user.provider.concrete.database_users'), 1 => new \Symfony\Component\Security\Core\User\InMemoryUserProvider()), 'main', $a, $this->get('debug.event_dispatcher', ContainerInterface::NULL_ON_INVALID_REFERENCE), $c), 2 => new \Symfony\Component\Security\Guard\Firewall\GuardAuthenticationListener($this->get('security.authentication.guard_handler'), $d, 'main', array(0 => $e), $a), 3 => new \Symfony\Component\Security\Http\Firewall\AnonymousAuthenticationListener($b, '58bf0ca5c3eab7.96569345', $a, $d), 4 => new \Symfony\Component\Security\Http\Firewall\AccessListener($b, $this->get('debug.security.access.decision_manager'), $g, $d)), new \Symfony\Component\Security\Http\Firewall\ExceptionListener($b, $c, new \Symfony\Component\Security\Http\HttpUtils($f, $f), 'main', $e, NULL, NULL, $a, false));
     }
 
     /**
@@ -3733,7 +3733,7 @@ class appDevDebugProjectContainer extends Container
      */
     protected function getSecurity_Authentication_ManagerService()
     {
-        $this->services['security.authentication.manager'] = $instance = new \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager(array(0 => new \Symfony\Component\Security\Guard\Provider\GuardAuthenticationProvider(array(0 => $this->get('app.form_login_authenticator')), $this->get('security.user.provider.concrete.database_users'), 'main', $this->get('security.user_checker.main')), 1 => new \Symfony\Component\Security\Core\Authentication\Provider\AnonymousAuthenticationProvider('58bda8ed25b6f2.78595253')), true);
+        $this->services['security.authentication.manager'] = $instance = new \Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager(array(0 => new \Symfony\Component\Security\Guard\Provider\GuardAuthenticationProvider(array(0 => $this->get('app.form_login_authenticator')), $this->get('security.user.provider.concrete.database_users'), 'main', $this->get('security.user_checker.main')), 1 => new \Symfony\Component\Security\Core\Authentication\Provider\AnonymousAuthenticationProvider('58bf0ca5c3eab7.96569345')), true);
 
         $instance->setEventDispatcher($this->get('debug.event_dispatcher'));
 
