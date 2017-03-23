91b83f102bbabbcc9560106c6d6c619a3f465996
michael.reilhac <michael.reilhac@live.fr>
2017-03-23 15:13:51 -0400

    dynamic-comment

--- vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/public/js/comments.js
+++ vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/public/js/comments.js
@@ -4,15 +4,20 @@ $(document).ready(function(){
     var comment = $(this).find('textarea[name="comment"]').val();
     var idVideo =  $("video").attr('id');
     var idChannel = $('a[name="channel"]').attr('id');
-    alert(idVideo+' - '+idChannel);
+    
     $.post('/web/bundles/framework/php/comments.php',{comment : comment, idChannel : idChannel, idVideo : idVideo},function(data, success){ 
       $('#comment').after(data);
+      $('button[name="delete_comment"]').on('click',function(){
+      var idComment = $(this).prev().val();
+      $('#'+idComment).remove();
+      $.post('/web/bundles/framework/php/delete_comment.php',{idComment: idComment});
     });
    });
-   $('button[name="delete_comment"]').on('click',function(){
-    var idComment = $(this).prev().val();
-    $('#'+idComment).remove();
-    $.post('/web/bundles/framework/php/delete_comment.php',{idComment: idComment});
-   }); 
-});
+  });
 
+   $('button[name="delete_comment"]').on('click',function(){
+      var idComment = $(this).prev().val();
+      $('#'+idComment).remove();
+      $.post('/web/bundles/framework/php/delete_comment.php',{idComment: idComment});
+    }); 
+});
--- vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/public/php/comments.php
+++ vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/public/php/comments.php
@@ -40,12 +40,23 @@
 			$stmt->bindParam(':msg',$messageComment);
 			$stmt->bindParam(':dateC',$date);
       $stmt->execute();  
+				
+			$sql = "SELECT idComment FROM Comment WHERE idNostreamer = :idN and idVideo=:idV and idChannel=:idC and messageComment=:msg ";
+		  $stmt = $db->prepare($sql);
+		  $stmt->bindParam(':idN',$idNostreamer);
+			$stmt->bindParam(':idC',$idChannel);
+      $stmt->bindParam(':idV',$idVideo); 	
+			$stmt->bindParam(':msg',$messageComment);	
+		  $stmt->execute(); 
+		  $result = $stmt->fetchAll();
+			$idComment = $result[0]['idComment'];  
+				
       echo'
-      <div style="background-color: #dddddd;border: 1px ridge black;padding:10px;margin-left:20px;margin-bottom:10px;width:80%;overflow:hidden;" class="callout" data-closable="">
+      <div id="'.$idComment.'" style="background-color: #dddddd;border: 1px ridge black;padding:10px;margin-left:20px;margin-bottom:10px;width:80%;overflow:hidden;" class="callout" data-closable="">
         <div style="border-bottom: solid 1px;">
           <img style="width:30px;height:30px;" src="met.jpg">
-            <span style="margin-left:10px;">'.$pseudoNostreamer.'<small> le 2017-03-23 13:10:29</small></span>
-              <input name="idComment" value="132" hidden="">
+            <span style="margin-left:10px;">'.$pseudoNostreamer.'<small> le '.$date.'</small></span>
+              <input name="idComment" value="'.$idComment.'" hidden="">
               <button style="outline:none;" type="submit" name="delete_comment" class="close-button" aria-label="Dismiss alert" data-close="">
                 <span aria-hidden="true">×</span>
               </button>
